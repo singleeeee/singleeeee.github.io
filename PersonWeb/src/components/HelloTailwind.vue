f009b802bae733981de824de9c3e3b3e28e0e4c9
Single <1197826934@qq.com>
2024-01-29 15:04:47 +0800

    feat:'背景图、备案信息'

---
+++ PersonWeb/src/components/HelloTailwind.vue
@@ -0,0 +1,8 @@
+<template>
+
+  <div class="bg-black text-center">hello-tailwind</div>
+
+</template>
+<script lang="ts" setup>
+
+</script>
---
+++ PersonWeb/src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <header class="text-center text-xl mt-8"> Single's 个人网站</header>
+  <footer class="flex p-3 pb-5 flex-col justify-center absolute bottom-0 w-full h-16 bg-black opacity-70 text-gray-200 text-center text-xs" >
+    <div>Copyright © 2024-2025 Singleeeee. All Rights Reserved.</div>
+     <a class="hover:text-blue-400" href="http://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">互联网ICP备案:粤ICP备2024172166</a>
+    <div>作者：Single | 邮箱：1197826934@qq.com </div>
+  </footer>
+
+</template>
+<script lang="ts" setup >
+
+</script>
+<style scoped>
+
+</style>
